82169b77c10d4ce630e401f6ee15422cf39a06ee
Ilusca <dilusca@techteam.com>
2022-11-18 11:18:46 +0200

    commit before switching laptops

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  console.log('config00');
-  return ({
-    verbose: true,
-    setupFilesAfterEnv: [
-      '<rootDir>/setup.js',
-    ],
-  });
-};
--- src/features/users/UserForm/__tests__/UserForm.test.jsx
+++ src/features/users/UserForm/__tests__/UserForm.test.jsx
@@ -3,22 +3,22 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import UserForm from '../UserForm';
 
 const mockPostUser = jest.fn();
-jest.mock('../../../../common/services/usersApi', () => jest.fn().mockImplementation(
-  () => ({ postUser: mockPostUser }),
-));
+jest.mock('../../../../common/services/usersApi', () => jest.fn().mockImplementation(() => ({ postUser: mockPostUser })));
 
-describe.skip('Render form', () => {
+describe('Render form', () => {
   it.skip('should render correctly', () => {
-    const mockSubmit = jest.fn();
-    const { baseElement } = render(<UserForm onCreateUser={mockSubmit} />);
+    const { baseElement } = render(<UserForm onCreateUser={mockPostUser} />);
     expect(baseElement).toMatchSnapshot();
   });
 
   it('creates new user', async () => {
-    const mockSubmit = jest.fn();
+    // const mockSubmit = jest.fn();
+    const onCreateUser = (items) => {
+      console.log(items);
+    };
     const {
       getByPlaceholderText, getByTestId,
-    } = render(<UserForm onCreateUser={mockSubmit} />);
+    } = render(<UserForm onCreateUser={onCreateUser} />);
 
     fireEvent.change(getByPlaceholderText('First Name'), { target: { value: 'Test' } });
     fireEvent.change(getByPlaceholderText('Last Name'), { target: { value: 'Test1' } });
@@ -27,7 +27,7 @@ describe.skip('Render form', () => {
 
     fireEvent.click(getByTestId('submit-button'));
     await waitFor(async () => {
-      expect(mockSubmit).toHaveBeenCalledWith({});
+      expect(mockPostUser).toHaveBeenCalled();
     });
   });
 });
