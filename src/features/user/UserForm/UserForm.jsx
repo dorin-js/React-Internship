9d4acd79d01ae78a3c0594e3b27d4d75c4d177cb
Dorin Ilușca <dorin.ilusca@stefanini.com>
2022-11-30 01:48:42 +0200

    feat: React hook form validation

--- src/features/user/UserForm/UserForm.jsx
+++ src/features/user/UserForm/UserForm.jsx
@@ -1,97 +1,152 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable no-useless-escape */
+import React, { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
+import { useForm } from 'react-hook-form';
 import classes from './UserForm.module.css';
 import { Button } from '../../../common/components/Button';
 import { useCreateNewUserMutation, useUpdateUserMutation } from '../../../services/api/apiService';
 import { Snackbar } from '../../../common/components/Snackbar';
 
-const defaultFormData = {
-  id: '',
-  name: '',
-  lastname: '',
-  email: '',
-  birth: '',
-};
-
 const UserForm = ({ isEditing, user }) => {
-  const [form, setForm] = useState(defaultFormData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
 
   useEffect(() => {
-    if (user?.name) {
-      setForm(user);
+    if (isEditing) {
+      const fields = ['firstname', 'lastname', 'email', 'birthDate', 'id'];
+      fields.forEach((field) => setValue(field, user[field]));
     }
-  }, [user]);
+  }, [isEditing, setValue, user]);
 
   const [updateUser, {
     isLoading: isUpdatingInProgress,
-    isError: isUpdateUserInfoError,
-    error: updateUserInfoError,
+    isError: isUpdateUserError,
+    error: updateUserError,
   }] = useUpdateUserMutation();
 
   const [createNewUser, {
-    isSuccess,
     isLoading: isCreatingInProgress,
     isError: isCreateNewUserError,
     error: createNewUserError,
   }] = useCreateNewUserMutation();
 
-  const onValueChanged = (value) => {
-    setForm((prevState) => ({ ...prevState, ...value }));
-  };
-
-  useEffect(() => {
-    if (isSuccess) {
-      setForm(defaultFormData);
+  const onSubmit = (data) => {
+    if (isEditing) {
+      updateUser(data);
+    } else {
+      createNewUser(data);
     }
-  }, [isSuccess]);
+  };
 
   return (
     <>
       <form
+        onSubmit={handleSubmit(onSubmit)}
         name="form"
         className={classes.createUserForm}
       >
-        <input
-          placeholder="First Name"
-          value={form.name}
-          onChange={(e) => onValueChanged({ name: e.target.value })}
+        <div className={classes.inputContainer}>
+          <input
+            placeholder="First Name"
+            {...register('firstname', {
+              required: {
+                value: true,
+                message: 'First Name is required',
+              },
+              maxLength: {
+                value: 30,
+                message: 'The input is too long',
+              },
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: 'Name shoud contain letters only',
+              },
+            })}
+          />
+          <p>{errors?.firstname?.message}</p>
+        </div>
+        <div className={classes.inputContainer}>
+          <input
+            placeholder="Last Name"
+            {...register('lastname', {
+              required: {
+                value: true,
+                message: 'Last Name is required',
+              },
+              maxLength: {
+                value: 30,
+                message: 'The input is too long',
+              },
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: 'Name shoud contain letters only',
+              },
+            })}
+          />
+          <p>{errors?.lastname?.message}</p>
+        </div>
+        <div className={classes.inputContainer}>
+          <input
+            type="email"
+            data-testid="email-input-field"
+            placeholder="Email"
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'Email is required',
+              },
+              maxLength: {
+                value: 30,
+                message: 'The input is too long',
+              },
+              pattern: {
+                value: /^\w+([\.-]?\w+)*@\w+([\.]\w+)(\.\w{2,5})?$/,
+                message: 'Incorrect email format',
+              },
+            })}
+          />
+          <p>{errors?.email?.message}</p>
+        </div>
+        <div className={classes.inputContainer}>
+          <input
+            type="date"
+            placeholder="Date of birth"
+            min="1900-01-01"
+            max={new Date().toISOString().split('T')[0]}
+            {...register('birthDate', {
+              required: {
+                value: true,
+                message: 'Birth date is required',
+              },
+            })}
+          />
+          <p>{errors?.birthDate?.message}</p>
+        </div>
+        <Button
+          type="submit"
+          aria-label={isEditing ? 'Update user' : 'Create user'}
+          value={(isUpdatingInProgress || isCreatingInProgress) ? 'Saving...' : 'Save'}
         />
-        <input
-          placeholder="Last Name"
-          value={form.lastname}
-          onChange={(e) => onValueChanged({ lastname: e.target.value })}
-        />
-        <input
-          type="email"
-          data-testid="email-input-field"
-          placeholder="Email"
-          value={form.email}
-          onChange={(e) => onValueChanged({ email: e.target.value })}
-        />
-        <input
-          type="date"
-          placeholder="Date of birth"
-          value={form.birth}
-          onChange={(e) => onValueChanged({ birth: e.target.value })}
-        />
-        {
-          isEditing
-            ? (
-              <Button
-                value={isUpdatingInProgress ? 'Saving...' : 'Save'}
-                onClick={() => updateUser(form)}
-              />
-            )
-            : (
-              <Button
-                value={isCreatingInProgress ? 'Creating...' : 'Create'}
-                onClick={() => createNewUser(form)}
-              />
-            )
-        }
       </form>
-      {isCreateNewUserError && <Snackbar type="error" message={createNewUserError.data.error} timeout={4000} />}
-      {isUpdateUserInfoError && <Snackbar type="error" message={updateUserInfoError.data.error} timeout={4000} />}
+      {
+        (isCreateNewUserError || isUpdateUserError)
+        && (
+          <Snackbar
+            type="error"
+            timeout={4000}
+            message={
+              createNewUserError?.data?.error
+              || createNewUserError?.error
+              || updateUserError?.data?.error
+              || updateUserError?.error
+            }
+          />
+        )
+      }
     </>
   );
 };
@@ -99,10 +154,10 @@ const UserForm = ({ isEditing, user }) => {
 UserForm.propTypes = {
   user: PropTypes.PropTypes.shape({
     id: PropTypes.string,
-    name: PropTypes.string,
+    firstname: PropTypes.string,
     lastname: PropTypes.string,
     email: PropTypes.string,
-    birth: PropTypes.string,
+    birthDate: PropTypes.string,
   }),
   isEditing: PropTypes.bool,
 };
